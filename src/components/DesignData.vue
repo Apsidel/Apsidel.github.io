fe4e4fa6b8cce4911ed6426689df9d36447d3e55
Kent Soledispa <kent.soledispa@richmond.edu>
2024-12-29 22:50:57 -0500

    Replaced metadata field with design_data in design_data.json
    - design_data contains data pertaining to the design (programs, file
      type, resolution, color mode, etc.)
    - More changes to the layout in GalleryItem.vue

---
+++ src/components/DesignData.vue
@@ -0,0 +1,60 @@
+<script setup>
+const props = defineProps({
+  design_data: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+<template>
+  <table>
+    <tbody>
+      <tr>
+        <td>Programs</td>
+        <td>{{ props.design_data.programs }}</td>
+      </tr>
+      <tr>
+        <td>File Type</td>
+        <td>{{ props.design_data.file_type }}</td>
+      </tr>
+      <tr>
+        <td>Dimensions</td>
+        <td>{{ props.design_data.dimensions }}</td>
+      </tr>
+      <tr>
+        <td>Resolution</td>
+        <td>{{ props.design_data.resolution }}</td>
+      </tr>
+      <tr>
+        <td>Color Mode</td>
+        <td>{{ props.design_data.color_mode }}</td>
+      </tr>
+      <tr>
+        <td>Design Type</td>
+        <td>{{ props.design_data.design_type }}</td>
+      </tr>
+      <tr v-if="design_data.brand">
+        <td>Brand</td>
+        <td>{{ props.design_data.brand }}</td>
+      </tr>
+      <tr v-if="design_data.product">
+        <td>Product</td>
+        <td>{{ props.design_data.product }}</td>
+      </tr>
+      <tr v-if="design_data.event">
+        <td>Event</td>
+        <td>{{ props.design_data.event }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+<style scoped>
+table {
+  padding-top: 15px;
+  width: 100%;
+}
+
+tr {
+  line-height: 2.2em;
+}
+</style>
--- src/components/GalleryItem.vue
+++ src/components/GalleryItem.vue
@@ -1,4 +1,6 @@
 <script setup>
+import DesignData from './DesignData.vue'
+
 const props = defineProps({
   image: {
     type: Object,
@@ -11,35 +13,51 @@ const props = defineProps({
     <section id="image_container">
       <img :src="'../../src/assets/images/' + image.url" :alt="image.title" />
     </section>
-    <section id="info_container"></section>
-    <section id="text_container">
+    <section id="title_container">
       <h1>{{ props.image.title }}</h1>
       <ul>
         <li
-          id="color-square"
-          v-for="color in image.colors"
+          class="color-square"
+          v-for="color in image.colors.slice(0, 12)"
           :key="color"
           :style="'background-color:' + color"
         ></li>
       </ul>
-      <div id="description">{{ props.image.description }}</div>
     </section>
+    <section id="details_container">
+      <DesignData :design_data="image.design_data" />
+      <ul>
+        <li
+          class="color-square"
+          v-for="color in image.colors.slice(0, 12).reverse()"
+          :key="color"
+          :style="'background-color:' + color"
+        ></li>
+      </ul>
+    </section>
+    <section id="description_container">
+      <div class="description">{{ props.image.description }}</div>
+    </section>
+    <section id="mockup"><h1>Mock-up</h1></section>
   </body>
 </template>
 
 <style scoped>
 body {
   display: grid;
-  grid-template-columns: 3.2fr 1.6fr 2.2fr;
-  grid-template-areas: 'image info text';
+  grid-template-columns: 2fr 2fr 2fr 1fr;
+  grid-template-rows: 1fr 6fr;
+  grid-template-areas:
+    'image title   description mockup'
+    'image details description mockup';
+  column-gap: 25px;
+  height: 100vh;
 }
 
-body > #image_container {
+#image_container {
   grid-area: image;
-}
-
-body > #text_container {
-  grid-area: info;
+  max-width: 900px;
+  height: auto;
 }
 
 img {
@@ -47,43 +65,10 @@ img {
   max-height: 700px;
 }
 
-section {
-  display: inline-block;
-  vertical-align: top;
-}
-
-#image_container {
-  max-width: 900px;
-  height: auto;
-}
-
-#info_container {
-  padding-left: 60px;
-}
-
-#text_container {
-  max-width: 340px;
-  padding-left: 25px;
+#title_container {
+  grid-area: title;
   line-height: 2.6em;
-  text-align: left;
-}
-
-#description {
-  line-height: 1.2em;
-  margin-top: 10px;
-  padding-top: 10px;
-  /* border-top: 1.5px solid; */
-}
-
-ul {
-  line-height: 1em;
-}
-
-#color-square {
-  display: inline-block;
-  margin-right: 2px;
-  height: 20px;
-  width: 40px;
+  /* max-width: 340px; */
 }
 
 h1 {
@@ -91,5 +76,47 @@ h1 {
   margin-bottom: 10px;
   padding-bottom: 10px;
   border-bottom: 2px solid;
+}
+
+ul {
+  line-height: 1em;
+}
+
+.color-square {
+  display: inline-block;
+  margin-right: 2px;
+  height: 20px;
+  width: 38px;
+}
+
+#description_container {
+  grid-area: description;
+}
+
+.description {
+  line-height: 1.2em;
+  text-align: justify;
+}
+
+#details_container {
+  grid-area: details;
+}
+
+#details_container ul {
+  padding-top: 16px;
+  text-align: right;
+}
+
+#mockup {
+  grid-area: mockup;
+  height: 700px;
+  text-align: center;
+  writing-mode: vertical-lr;
+  background-color: black;
+}
+
+#mockup h1 {
+  color: white;
+  border-style: none;
 }
 </style>
