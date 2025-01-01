e26a08ffacd26e29116254e36e9467eee5c4601c
Kent Soledispa <kent.soledispa@richmond.edu>
2025-01-01 17:43:01 -0500

    Added ColorStrip.vue
    - Takes an array of Hex colors and produces a series of divs with the
      corresponding color as the background color.

---
+++ src/components/ColorStrip.vue
@@ -0,0 +1,24 @@
+<script setup>
+const props = defineProps({
+  colors: {
+    type: Array,
+    required: true
+  }
+})
+</script>
+<template>
+  <ul>
+    <li v-for="color in props.colors" :key="color" :style="'background-color:' + color"></li>
+  </ul>
+</template>
+<style scoped>
+ul {
+  line-height: 1em;
+}
+li {
+  display: inline-block;
+  margin-right: 2px;
+  height: 20px;
+  width: 38px;
+}
+</style>
