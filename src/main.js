a493bb411c4ffc57d62bbf1c7a7413eb1c3d4281
Kent Soledispa <kent.soledispa@richmond.edu>
2025-10-11 18:40:24 -0400

    Imported new FontAwesome icons.

--- src/main.js
+++ src/main.js
@@ -14,9 +14,27 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faSquareJs } from '@fortawesome/free-brands-svg-icons'
+import { faJava } from '@fortawesome/free-brands-svg-icons'
+import { faVuejs } from '@fortawesome/free-brands-svg-icons'
+import { faGitAlt } from '@fortawesome/free-brands-svg-icons'
+import { faJenkins } from '@fortawesome/free-brands-svg-icons'
+import { faHtml5 } from '@fortawesome/free-brands-svg-icons'
+import { faCss3Alt } from '@fortawesome/free-brands-svg-icons'
+import { faNodeJs } from '@fortawesome/free-brands-svg-icons'
+import { faFigma } from '@fortawesome/free-brands-svg-icons'
+import { faTerminal } from '@fortawesome/free-solid-svg-icons'
+import { faWindowMaximize } from '@fortawesome/free-solid-svg-icons'
+import { faWindowRestore } from '@fortawesome/free-solid-svg-icons'
+import { faCode } from '@fortawesome/free-solid-svg-icons'
+import { faCodeCompare } from '@fortawesome/free-solid-svg-icons'
+import { faLeaf } from '@fortawesome/free-solid-svg-icons'
+import { faSquareCheck } from '@fortawesome/free-solid-svg-icons'
 
 /* add icons to the library */
-library.add(faLinkedin, faGithub, faEnvelope);
+library.add(faLinkedin, faGithub, faEnvelope, faSquareJs, faJava, faVuejs, faGitAlt,
+    faJenkins, faHtml5, faCss3Alt, faNodeJs, faFigma, faTerminal, faWindowMaximize,
+    faWindowRestore, faCode, faCodeCompare, faLeaf, faSquareCheck);
 
 const app = createApp(App)
 
